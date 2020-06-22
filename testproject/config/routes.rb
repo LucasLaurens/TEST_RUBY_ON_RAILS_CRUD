6cf31640b04af39de0e8a2aec38593bd85091243
LucasLaurens <lucaslaurens.74@gmail.com>
2020-06-22 18:09:22 +0200

    [Back][Front] display all projects

--- testproject/config/routes.rb
+++ testproject/config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Get index of crud
+  root to: 'projects#index'
+  get '/projects', to: 'projects#index', as: 'projects'
+
+
+
+  resources :projects
+  # only one task
+  # get '/read/:id', to: 'crud#show'
+  # create a task
+  # post '/create', to: 'crud#create'
+  # edit a task
+  # put '/edit', to: 'crud#edit'
+  # delete a task
+  # # delete '/delete', to: 'crud#delete'
 end
